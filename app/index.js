1fb194ef843ebe623b35e3825926138613de6ea8
mharis0014 <mh972240@gmail.com>
2024-08-17 12:32:11 +0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,103 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "../constants/theme";
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      />
+      {/* linear gradient */}
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "#FFFFFF",
+            "#FFFFFF",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Pixels
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            Every Pixel Tells a Story
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable style={styles.startButton}>
+              <Text style={styles.startText}>Start Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImage: {
+    height: hp(100),
+    width: wp(100),
+    position: "absolute",
+  },
+  gradient: {
+    height: hp(65),
+    width: wp(100),
+    position: "absolute",
+    bottom: 0,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+  startText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1,
+  },
+});
+
+export default WelcomeScreen;
---
+++ constants/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    black: "#000000",
+    grayBG: "#E5E5E5",
+    neutral: (opacity) => `rgba(10, 10, 10, ${opacity})`,
+  },
+  fontWeights: {
+    medium: "500",
+    semibold: "700",
+    bold: "900",
+  },
+  radius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  },
+};
