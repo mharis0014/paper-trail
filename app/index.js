f615b0fdeb56255e2e3c8df2b767ea90d18e6868
mharis0014 <mh972240@gmail.com>
2024-08-29 18:08:32 +0500

    home screen

--- app/index.js
+++ app/index.js
@@ -5,8 +5,11 @@ import { hp, wp } from "../helpers/common";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { theme } from "../constants/theme";
+import { useRouter } from "expo-router";
 
 const WelcomeScreen = () => {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -42,7 +45,10 @@ const WelcomeScreen = () => {
             Every Pixel Tells a Story
           </Animated.Text>
           <Animated.View entering={FadeInDown.delay(600).springify()}>
-            <Pressable style={styles.startButton}>
+            <Pressable
+              onPress={() => router.push("home")}
+              style={styles.startButton}
+            >
               <Text style={styles.startText}>Start Explore</Text>
             </Pressable>
           </Animated.View>
